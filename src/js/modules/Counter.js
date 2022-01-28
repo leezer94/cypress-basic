47aacc1fbebf1e725a1468f2e4da4472ea761da7
leezer94 <2kunhee94@gmail.com>
2022-01-28 16:24:26 +0900

    feat: add increase, decrease function when user clicks on plus,minus button each function will execute

--- src/js/modules/Counter.js
+++ src/js/modules/Counter.js
@@ -11,8 +11,32 @@ export default function Counter({ $app }) {
         </div>`;
   };
 
+  const initEvent = () => {
+    const plusButton = document.querySelector('.plus-button');
+    const minusButton = document.querySelector('.minus-button');
+    const countDisplay = document.querySelector('.count-display');
+
+    const increaseValue = () => {
+      if (countDisplay.value >= 12) {
+        return;
+      }
+      countDisplay.value = String(Number(countDisplay.value) + 1);
+    };
+
+    const decreaseValue = () => {
+      if (countDisplay.value <= 8) {
+        return;
+      }
+      countDisplay.value = String(Number(countDisplay.value) - 1);
+    };
+
+    plusButton.addEventListener('click', increaseValue);
+    minusButton.addEventListener('click', decreaseValue);
+  };
+
   const init = () => {
     render();
+    initEvent();
   };
 
   init();
